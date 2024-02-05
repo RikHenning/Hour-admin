ed99f86d18acdcf19ae050516a585c8f680b492e
RikHenning <rikhenning@gmail.com>
2024-02-05 08:15:14 +0100

    05-02-2024 Little something

--- hour-admin/hourtracking/src/components/EmployeeList.vue
+++ hour-admin/hourtracking/src/components/EmployeeList.vue
@@ -1,49 +1,56 @@
 <template>
-    <div>
-      <h2>Employee List</h2>
-      <ul>
-        <li v-for="employee in employees" :key="employee.id">
-          {{ employee.firstName }} {{ employee.lastName }} - Total Hours: {{ employee.totalHours }}
-          <button @click="removeEmployee(employee.id)">Remove</button>
-        </li>
-      </ul>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        employees: [],
-      };
-    },
-    mounted() {
-      this.loadEmployeesFromLocalStorage();
-    },
-    methods: {
-      loadEmployeesFromLocalStorage() {
-        // Iterate through Local Storage keys and retrieve employee data
-        for (let i = 0; i < localStorage.length; i++) {
-          const key = localStorage.key(i);
-          if (key && key.startsWith("employee-")) {
-            const employeeDataString = localStorage.getItem(key);
-            if (employeeDataString) {
-              const employeeData = JSON.parse(employeeDataString);
-              this.employees.push(employeeData);
-            }
+  <div>
+    <h2>Employee List</h2>
+    <ul>
+      <li v-for="employee in employees" :key="employee.id">
+        {{ employee.firstName }} {{ employee.lastName }} - Total Hours: {{ employee.totalHours }}
+        <button @click="removeEmployee(employee.id)">Remove</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      employees: [],
+    };
+  },
+  mounted() {
+    this.loadEmployeesFromLocalStorage();
+    window.addEventListener('storage', this.handleStorageChange);
+  },
+  beforeUnmount() {
+    window.removeEventListener('storage', this.handleStorageChange);
+  },
+  methods: {
+    loadEmployeesFromLocalStorage() {
+      for (let i = 0; i < localStorage.length; i++) {
+        const key = localStorage.key(i);
+        if (key && key.startsWith("employee-")) {
+          const employeeDataString = localStorage.getItem(key);
+          if (employeeDataString) {
+            const employeeData = JSON.parse(employeeDataString);
+            this.employees.push(employeeData);
           }
         }
-      },
-      removeEmployee(employeeId) {
-        // Remove employee data from Local Storage and update the list
-        localStorage.removeItem(`employee-${employeeId}`);
-        this.employees = this.employees.filter(employee => employee.id !== employeeId);
-      },
+      }
     },
-  };
-  </script>
-  
-  <style scoped>
+    removeEmployee(employeeId) {
+      localStorage.removeItem(`employee-${employeeId}`);
+      this.employees = this.employees.filter(employee => employee.id !== employeeId);
+    },
+    handleStorageChange(event) {
+      if (event.key && event.key.startsWith("employee-")) {
+        this.loadEmployeesFromLocalStorage();
+        this.$emit('local-storage-updated');
+      }
+    },
+  },
+};
+</script>
 
-  </style>
-  
+<style scoped>
+
+</style>
