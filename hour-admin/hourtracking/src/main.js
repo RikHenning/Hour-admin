103d076fe6c617f517b1ce09ed95fd10ba803948
Barra Abd Al Fattah <barra@databrydge.com>
2024-02-23 19:41:08 +0100

    test

--- hour-admin/hourtracking/src/main.js
+++ hour-admin/hourtracking/src/main.js
@@ -2,18 +2,23 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router/index';
 import './assets/style.css';
-import { VueFinalModal } from 'vue-final-modal';
-import EmployeeList from "./components/EmployeeList.vue";
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { createVfm } from 'vue-final-modal';
+import EmployeeList from "./components/EmployeeList.vue";
+
 
 
 const app = createApp(App);
 
 app.use(router);
-app.use(VueFinalModal);
+// app.use(VueFinalModal);
+
+const vfm = createVfm();
+app.use(vfm);
 
 app.component('EmployeeList', EmployeeList);
 
 app.mount("#app");
+
 
